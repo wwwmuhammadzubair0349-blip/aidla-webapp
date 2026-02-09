04edc1aa30636e3719a765a7e8badb6b95004f8e
Muhammad Zubair Afridi <www.muhammadzubair0349@gmail.com>
2026-02-09 19:41:19 +0400

    op

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
-import Layout from "./src/Components/Layout";
+import Layout from "../Components/Layout";
 
 import RequireAuth from "../src/Components/RequireAuth";
 import RequireAdmin from "../src/Components/Requireadmin";
